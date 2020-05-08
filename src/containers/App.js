c44bfb6a490f25d5ad9c72979d2f43c4c99e7136
dcca <dcca12@gmail.com>
2020-05-08 20:04:08 -0300

    small fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,8 +32,8 @@ class app extends Component {
 		});
 	};
 	render() {
-		const filteredRobots = this.state.robots.filter((robots) => {
-			return robots.name
+		const filteredRobots = this.state.robots.filter((robot) => {
+			return robot.name
 				.toLowerCase()
 				.includes(this.state.searchfield.toLowerCase());
 		});
